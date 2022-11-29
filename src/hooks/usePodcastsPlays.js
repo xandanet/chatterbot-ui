78e5610f6a72611a9d76d14f932a7a991c17dff2
Jason Green <jasongreen@gmx.com>
2022-11-29 16:34:39 +0000

    Added layout and more charts

--- src/hooks/usePodcastsPlays.js
+++ src/hooks/usePodcastsPlays.js
@@ -1,18 +1,18 @@
 import {useQuery} from 'react-query';
 
-const fetchInteventions = async ({queryKey}) => {
+const fetchPlays = async ({queryKey}) => {
     const { episodeId } = queryKey?.[1];
-    const response = await fetch('https://api.chatterbot.hostings.co.uk/v1/podcasts/interventions?id=' + episodeId || '')
+    const response = await fetch('https://api.chatterbot.hostings.co.uk/v1/plays/per-day?podcast_id=' + episodeId || '')
     if (!response.ok) {
         throw new Error('Network response was not ok')
     }
     return response.json()
 }
 
-export default function useInterventions({episodeId}) {
+export default function usePlays({episodeId}) {
     const query = useQuery(
-        ['interventions', {episodeId}],
-        fetchInteventions,
+        ['plays-per-day', {episodeId}],
+        fetchPlays,
         {
             enabled: !!episodeId
         }
--- src/pages/Analytics/Episode.jsx
+++ src/pages/Analytics/Episode.jsx
@@ -1,103 +1,144 @@
 import {useState} from 'react';
-import { useParams } from 'react-router-dom'
-import { BarChart, PieChart, Pie, Tooltip, CartesianGrid, XAxis, YAxis, Legend, Bar } from 'recharts';
+import {useParams} from 'react-router-dom'
+import {BarChart, PieChart, Pie, Tooltip, CartesianGrid, XAxis, YAxis, Legend, Bar} from 'recharts';
+import {Col, Row} from 'antd';
 
-import { humanizer } from 'humanize-duration'
+import {humanizer} from 'humanize-duration'
 
 import {podcasts} from '../../data/podcasts';
-import usePodcastInterventions from '../../hooks/usePodcastsInterventions.js';
+import usePodcastInterventions from '../../hooks/usePodcastsInterventions';
+import usePodcastsPlays from '../../hooks/usePodcastsPlays';
 
 export default function Episode() {
-	const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-	let { episodeId } = useParams();
-	const [interventions, interventionsQuery] = usePodcastInterventions({episodeId});
+  let {episodeId} = useParams();
+  const [interventions, interventionsQuery] = usePodcastInterventions({episodeId});
+  const [plays, playsQuery] = usePodcastsPlays({episodeId});
+  console.log('plays', plays)
 
-	const filteredPodcasts = podcasts.filter((podcast) => {
-		const title = podcast.title.toLowerCase();
-		return title.includes(searchTerm.toLowerCase());
-	});
+  const filteredPodcasts = podcasts.filter((podcast) => {
+    const title = podcast.title.toLowerCase();
+    return title.includes(searchTerm.toLowerCase());
+  });
 
-	const getInterventionsDurationData = () => {
-		let data = [];
-		if(interventions) {
-			interventions.forEach((element, index) => {
-				data.push({
-					name: `Speaker ${index + 1}`,
-					value: element.time_spoken
-				})
-			});
-		}
+  const getPodcastPlaysData = () => {
+    let data = [];
+    if (plays) {
+      plays.forEach((element, index) => {
+        data.push({
+          name: element.date,
+          value: element.count
+        })
+      });
+    }
 
-		return data;
-	}
-	const getSpeakerPopularityData = () => {
-		console.log(interventions, interventionsQuery);
-		let data = [];
-		if(interventions) {
-			interventions.forEach((element, index) => {
-				data.push({
-					name: `Speaker ${index + 1}`,
-					value: element.plays
-				})
-			});
-		}
+    return data;
+  }
 
-		return data;
-	}
+  const getInterventionsDurationData = () => {
+    let data = [];
+    if (interventions) {
+      interventions.forEach((element, index) => {
+        data.push({
+          name: `Speaker ${index + 1}`,
+          value: element.time_spoken
+        })
+      });
+    }
 
-	const RADIAN = Math.PI / 180;
-	const getInterventionsDurationLabels = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-		const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-		const x = cx + radius * Math.cos(-midAngle * RADIAN);
-		const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    return data;
+  }
 
-		const timeSpokenMillisecs = interventions[index].time_spoken
+  const getSpeakerPopularityData = () => {
+    let data = [];
+    if (interventions) {
+      interventions.forEach((element, index) => {
+        data.push({
+          name: `Speaker ${index + 1}`,
+          value: element.plays
+        })
+      });
+    }
 
-		return (
-			<text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-				{humanizer({ delimiter: " and ", round: true})(timeSpokenMillisecs)}
-			</text>
-		);
-	};
+    return data;
+  }
 
-	console.log(getSpeakerPopularityData())
+  const RADIAN = Math.PI / 180;
+  const getInterventionsDurationLabels = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-	return (
-		<div>
-			<h1 className="my-8">Episode Analytics</h1>
+    const timeSpokenMillisecs = interventions[index].time_spoken
 
-        <PieChart width={500} height={500}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={false}
-            data={getInterventionsDurationData()}
-            // outerRadius={250}
-            fill="#8884d8"
-						labelLine={false}
-            label={getInterventionsDurationLabels}
-          />
-          <Tooltip />
-        </PieChart>
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {humanizer({delimiter: " and ", round: true})(timeSpokenMillisecs)}
+      </text>
+    );
+  };
 
-			<BarChart
-          width={750}
-          height={500}
-          data={getSpeakerPopularityData()}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name"  label="Speaker" />
-          <YAxis  label="Plays"/>
-          <Tooltip />
-          <Bar dataKey="value" fill="#8884d8" />
-        </BarChart>
+  return (
+    <div>
+      <h1 className="my-8">Episode Analytics</h1>
 
-		</div>
-	);
+      <Row>
+        <Col>
+          <PieChart width={500} height={500}>
+            <Pie
+              dataKey="value"
+              isAnimationActive={false}
+              data={getInterventionsDurationData()}
+              // outerRadius={250}
+              fill="#8884d8"
+              labelLine={false}
+              label={getInterventionsDurationLabels}
+            />
+            <Tooltip/>
+          </PieChart>
+        </Col>
+        <Col>
+          <BarChart
+            width={750}
+            height={500}
+            data={getSpeakerPopularityData()}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="name" label=""/>
+            <YAxis label="Plays"/>
+            <Tooltip/>
+            <Bar dataKey="value" fill="#8884d8"/>
+          </BarChart>
+        </Col>
+      </Row>
+			<Row>
+				<Col>
+      <BarChart
+        width={750}
+        height={500}
+        data={getPodcastPlaysData()}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="name" label=""/>
+        <YAxis label="Plays"/>
+        <Tooltip/>
+        <Bar dataKey="value" fill="#8884d8"/>
+      </BarChart>
+				</Col>
+			</Row>
+    </div>
+  );
 }
