a9a8102af8d0072fcf3d7dd40ab3e06485f61afc
Jason Green <jason@xanda.net>
2022-11-30 11:54:57 +0000

    Updated api url

--- src/hooks/usePodcastsTopSearchesNoResults.js
+++ src/hooks/usePodcastsTopSearchesNoResults.js
@@ -1,7 +1,7 @@
 import {useQuery} from 'react-query';
 
 const fetchPodcastsTopSearchesNoResults = async () => {
-    const response = await fetch('https://api.chatterbot.hostings.co.uk/v1/podcasts/top-searches-no-results')
+    const response = await fetch('https://api.chatterbot.hostings.co.uk/v1/podcasts/top-searches-no-result')
     if (!response.ok) {
         throw new Error('Network response was not ok')
     }
