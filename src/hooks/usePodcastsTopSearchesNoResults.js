3dd2e96158f6a2ee2c15f23975cdc1be12458b81
Jason Green <jason@xanda.net>
2022-11-30 11:03:59 +0000

    Added no results table

---
+++ src/hooks/usePodcastsTopSearchesNoResults.js
@@ -0,0 +1,14 @@
+import {useQuery} from 'react-query';
+
+const fetchPodcastsTopSearchesNoResults = async () => {
+    const response = await fetch('https://api.chatterbot.hostings.co.uk/v1/podcasts/top-searches-no-results')
+    if (!response.ok) {
+        throw new Error('Network response was not ok')
+    }
+    return response.json()
+}
+
+export default function usePodcastsTopSearchesNoResults() {
+    const query = useQuery('topsearchesNoResults', fetchPodcastsTopSearchesNoResults)
+	  return [query?.data?.data, query];
+}
--- src/pages/Analytics/Search.jsx
+++ src/pages/Analytics/Search.jsx
@@ -3,12 +3,13 @@ import {useParams} from 'react-router-dom';
 import {Col, Row, Table} from 'antd';
 import usePodcastsTopSearchLocations from '../../hooks/usePodcastsTopSearchLocations';
 import usePodcastsTopSearches from '../../hooks/usePodcastsTopSearches.js';
+import usePodcastsTopSearchesNoResults from "../../hooks/usePodcastsTopSearchesNoResults";
 
 export default function Search() {
 	const [searchTerm, setSearchTerm] = useState('');
 
-	let {episodeId} = useParams();
 	const [topSearches, topSearchesQuery] = usePodcastsTopSearches();
+	const [topSearchesNoResults, topSearchesNoResultsQuery] = usePodcastsTopSearchesNoResults();
 	const [topSearchLocations, topSearchLocationsQuery] = usePodcastsTopSearchLocations();
 
 	const searchLocationColumns = [
@@ -67,19 +68,51 @@ export default function Search() {
 		return data;
 	};
 
+	const noResultsSearchColumns = [
+		{
+			title: 'Keywords',
+			dataIndex: 'keywords',
+			key: 'keywords',
+		},
+		{
+			title: 'Searches',
+			dataIndex: 'searches',
+			key: 'searches',
+		},
+	];
+
+	const noResultsSearchData = () => {
+		let data = [];
+		if (topSearchesNoResults) {
+			topSearchesNoResults.forEach((element, index) => {
+				data.push({
+					key: index,
+					searches: element.total,
+					keywords: element.keyword,
+				});
+			});
+		}
+
+		return data;
+	};
+
 	return (
 		<div className="centering">
 			<h1 className="my-8">Search Analytics</h1>
 
 			<Row gutter={20}>
-				<Col>
+				<Col span={8}>
 					<h2 className="text-xl mb-4">Top Searches</h2>
 					<Table dataSource={searchData()} columns={searchColumns} />
 				</Col>
-				<Col>
-					<h2 className="text-xl mb-4">Popular Search Locations</h2>
-					<Table dataSource={searchLocationData()} columns={searchLocationColumns} />
+				<Col span={8}>
+					<h2 className="text-xl mb-4">Top Search With No Results</h2>
+					<Table dataSource={noResultsSearchData()} columns={noResultsSearchColumns} />
 				</Col>
+        <Col span={8}>
+          <h2 className="text-xl mb-4">Popular Search Locations</h2>
+          <Table dataSource={searchLocationData()} columns={searchLocationColumns} />
+        </Col>
 			</Row>
 		</div>
 	);
